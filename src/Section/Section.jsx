473b6837c0b366be0b23a04dc64277cf452058cb
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-12-12 20:28:25 +0200

    Create Section.jsx

---
+++ src/Section/Section.jsx
@@ -0,0 +1,13 @@
+import { Title } from "./Section.styled";
+
+const Section = ({title, children}) => {
+    return(
+        <div>
+            <Title>{title}</Title>
+            {children}
+        </div>        
+    );
+};
+
+export default Section;
+
