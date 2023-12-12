d588d9fc37441cd27b4b9e8883e391c418035040
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-12-12 20:28:09 +0200

    Create Statistics.jsx

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Notification from "components/Notification/Notification";
+import { StatisticsWrapper, StatisticsInfo } from "./Statistics.styled";
+
+const Statistics = ({good, neutral, bad, total}) => {
+    return(
+        <div>
+            {total > 0 ? <StatisticsWrapper>
+                            <StatisticsInfo>Good: <span>{good}</span></StatisticsInfo>
+                            <StatisticsInfo>Neutral: <span>{neutral}</span></StatisticsInfo>
+                            <StatisticsInfo>Bad: <span>{bad}</span></StatisticsInfo>
+                            <StatisticsInfo>Total:<span>{total}</span></StatisticsInfo>
+                        </StatisticsWrapper>
+                  : <Notification message="There is no feedback"/>}                       
+        </div>
+    );
+}
+
+export default Statistics;
