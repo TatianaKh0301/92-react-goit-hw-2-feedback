c142385ea33a4a3fac2c7c30516e6355b10bac6d
TatianaKh0301 <92024678+TatianaKh0301@users.noreply.github.com>
2023-12-12 20:28:13 +0200

    Create Statistics.styled.jsx

---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StatisticsWrapper = styled.div`
+    padding: 5px;
+`;
+
+export const StatisticsInfo = styled.p`
+    padding-left: 5px;
+    text-align: center;
+    font-size: 20px;
+    font-weight: bold;
+`;
